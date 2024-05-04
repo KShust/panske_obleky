749c2b1d9115e9b2f07b68f8198a1dd042793803
Kateryna <shust.kateryna0@gmail.com>
2024-05-04 19:16:34 +0200

    cely program

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('funguju');
+const svg = document.getElementById('product-image');
+const colorButtons = document.querySelectorAll('.color-box');
+
+colorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const color = button.id;
+    switch (color) {
+      case 'bila':
+        svg.style.fill = 'white';
+        svg.style.stroke = 'black'
+        break;
+      case 'cerna':
+        svg.style.fill = 'black';
+        svg.style.stroke = 'white'
+        break;
+      case 'cervena':
+        svg.style.fill = 'red';
+        svg.style.stroke = 'black'
+        break;
+      case 'modra':
+        svg.style.fill = 'skyblue';
+        svg.style.stroke = 'black'
+        break;
+      default:
+        svg.style.fill = '#a6a6a6';
+        svg.style.stroke = 'black'
+    }
+  });
+});
